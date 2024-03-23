87bcbc75950f11716eac4f331a31e631a4ce8511
shdwraze <shdwrazedev@gmail.com>
2024-03-23 22:27:40 +0200

    create project

---
+++ src/main/java/com/sever0x/bookstore/GrpcBookstoreInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.sever0x.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcBookstoreInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcBookstoreInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sever0x/bookstore/GrpcBookstoreInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sever0x.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcBookstoreInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
